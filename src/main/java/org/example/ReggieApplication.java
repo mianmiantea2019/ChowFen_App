cf2204a6af5638d721f073aad669e0e16559579d
mianmiantea2019 <sguo321@gatech.edu>
2023-08-27 15:50:20 -0400

    upload previous project

---
+++ src/main/java/org/example/ReggieApplication.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Christy Guo
+ * @create 2023-03-28 12:00 PM
+ */
+
+@Slf4j
+@SpringBootApplication
+@ServletComponentScan
+@EnableTransactionManagement
+
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class,args);
+    }
+}
+
+
+
